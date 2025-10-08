8bc32ea246e1302264b586fca65dcc02ef64934d
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2025-10-08 04:10:23 +0200

    Modified Makefile to accomodate the new testing

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 SHELL := /bin/bash
+STDOUT := /dev/stdout
 
 CC := arm-linux-gnueabi-gcc
 CFLAGS := -g -static -march=armv6 -z noexecstack
@@ -9,9 +10,8 @@ OPTIMIZATION_LEVEL := 2
 RUN_COMMAND := qemu-arm -cpu arm1136 
 DEBUGGER := pwndbg
 
-TESTS_SRC_DIR:= "./tests"
-TESTS_EXP_DIR := "./tests/expected"
-TESTS_OUT_DIR := "./tests/output"
+TEST_FILE := tests/test.py
+TEST_COMMAND := python3 
 
 CFG_DOT_FILE := cfg/cfg.dot
 CFG_PDF_FILE := cfg/cfg.pdf
@@ -19,19 +19,21 @@ CFG_PNG_FILE := cfg/cfg.png
 
 INTERPRET := $(interpret)
 
-all: test
+all: call
 
-test:
+call:
 ifndef INTERPRET
 	$(MAKE) compile
 	$(MAKE) execute
 else
-	$(MAKE) interpret output_file=$(TESTS_OUT_DIR)/single_test.output
+	$(MAKE) interpret
 endif
 
 compile:
-	if [ $(test) ]; then\
-		python3 main.py -i $(test) -o $(ASSEMBLY) -O$(OPTIMIZATION_LEVEL);\
+	if [ $(input) ]; then\
+		python3 main.py -i $(input) -o $(ASSEMBLY) -O$(OPTIMIZATION_LEVEL);\
+	else\
+		printf "\n\e[31mPlease specify input file\e[0m\n";\
 	fi;
 	$(CC) $(CFLAGS) $(ASSEMBLY) runtime.c -o $(EXECUTABLE)
 	if [ ! $$? -eq 0 ]; then\
@@ -42,83 +44,30 @@ execute:
 	printf "\n\e[36mRUNNING\e[0m\n\n";
 	if [ $(dbg) ]; then\
 		$(RUN_COMMAND) -g 7777 $(EXECUTABLE);\
-	elif [ $(test) ]; then\
-		if [ ! -d $(TESTS_OUT_DIR) ]; then\
-			mkdir $(TESTS_OUT_DIR);\
-		fi;\
-		test_name=$$(basename "$(test)" .pl0);\
-		output_file=$(TESTS_OUT_DIR)/single_test.output;\
-		$(RUN_COMMAND) $(EXECUTABLE) > $$output_file;\
-		cat $$output_file;\
-		$(MAKE) -s check_output test_name=$$test_name output_file=$$output_file;\
 	else\
 		$(RUN_COMMAND) $(EXECUTABLE);\
 	fi;
 
 interpret:
-	if [ $(test) ]; then\
-		if [ ! -d $(TESTS_OUT_DIR) ]; then\
-			mkdir $(TESTS_OUT_DIR);\
-		fi;\
-		test_name=$$(basename "$(test)" .pl0);\
-		python3 main.py -i $(test) -o $(output_file) -O$(OPTIMIZATION_LEVEL) -I;\
+	if [ $(input) ]; then\
+		python3 main.py -i $(input) -o $(STDOUT) -O$(OPTIMIZATION_LEVEL) -I;\
 		if [ ! $$? -eq 0 ]; then\
 			printf "\n\e[31mThe program didn't interpret successfully\e[0m\n";\
 			exit 1;\
 		fi;\
-		if [ $(dontcheck) ]; then\
-			cat $(output_file);\
-		else\
-			cat $(output_file);\
-			$(MAKE) -s check_output test_name=$$test_name output_file=$(output_file);\
-		fi;\
 	else\
-		printf "\n\e[31mPlease specify input test\e[0m\n";\
+		printf "\n\e[31mPlease specify input file\e[0m\n";\
 	fi;
 
 testall:
-	if [ ! -d $(TESTS_OUT_DIR) ]; then\
-		mkdir $(TESTS_OUT_DIR);\
-	else\
-		rm -f $(TESTS_OUT_DIR)/*;\
-	fi;
 ifndef INTERPRET
-	for test_file in $(TESTS_SRC_DIR)/*.pl0; do\
-		test_name=$$(basename "$$test_file" .pl0);\
-		output_file=$(TESTS_OUT_DIR)/"$$test_name".output;\
-		$(MAKE) compile test="$$test_file" > /dev/null 2> $$output_file;\
-		return_value=$$(echo $$?);\
-		if [ "$$return_value" -eq "0" ]; then\
-			$(MAKE) execute | sed -n -e "4,\$$p" > $$output_file;\
-		else\
-			sed -i -e "\$$d" $$output_file;\
-			sed -n -i -e "\$$p" $$output_file;\
-		fi;\
-		$(MAKE) -s check_output test_name=$$test_name output_file=$$output_file;\
-	done;
+	$(TEST_COMMAND) $(TEST_FILE) -a -O$(OPTIMIZATION_LEVEL)
 else
-	for test_file in $(TESTS_SRC_DIR)/*.pl0; do\
-		test_name=$$(basename "$$test_file" .pl0);\
-		output_file=$(TESTS_OUT_DIR)/"$$test_name".output;\
-		$(MAKE) interpret test="$$test_file" output_file="$$output_file" dontcheck=True &> /dev/null;\
-		return_value=$$(echo $$?);\
-		if [ ! "$$return_value" -eq "0" ]; then\
-			sed -i -e "\$$d" $$output_file;\
-			sed -n -i -e "\$$p" $$output_file;\
-		fi;\
-		$(MAKE) -s check_output test_name=$$test_name output_file=$$output_file;\
-	done;
+	$(TEST_COMMAND) $(TEST_FILE) -a -O$(OPTIMIZATION_LEVEL) -I
 endif
 
-check_output:
-	expected_file=$(TESTS_EXP_DIR)/"$(test_name)".expected;\
-	if [ ! -f $$expected_file ]; then\
-		printf "\e[31mExpected output does not exist for test $(test_name)\e[0m\n";\
-	elif output=$$(diff -y -W 72 $(output_file) $$expected_file); then\
-		printf "\e[32mTest $(test_name) passed!\e[0m\n";\
-	else\
-		printf "\e[31mTest $(test_name) not passed\e[0m\n";\
-	fi;
+testallall:
+	$(TEST_COMMAND) $(TEST_FILE) -A
 
 clean:
 	rm $(ASSEMBLY) $(EXECUTABLE) $(CFG_DOT_FILE) $(CFG_PDF_FILE) $(CFG_PNG_FILE)
---
+++ tests/test.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+"""
+Testing platform
+"""
+
+from argparse import ArgumentParser
+
+import pytest
+
+default_args = ["--show-capture=no", "--no-header", "--tb=line"]
+
+
+def single_test(test, optimization_level, interpret, quiet):
+    args = ["-v"]
+
+    args += [] if quiet else ["-s"]
+    args += [f"--optimization_level={optimization_level}"]
+    args += ["-I"] if interpret else []
+
+    args += ["-k", test]
+
+    retvalue = pytest.main(args)
+    print(f"Return value: {retvalue}")
+
+
+def single_category(category, optimization_level, interpret):
+    args = default_args[:]
+
+    args += [f"--optimization_level={optimization_level}"]
+    args += ["-I"] if interpret else []
+
+    args += ["-k", category]
+
+    pytest.main(args)
+
+
+def single_directory(directory, optimization_level, interpret):
+    args = default_args[:]
+
+    args += [f"--optimization_level={optimization_level}"]
+    args += ["-I"] if interpret else []
+
+    args += [directory]
+
+    pytest.main(args)
+
+
+def test_all(optimization_level, interpret):
+    args = default_args[:]
+
+    args += [f"--optimization_level={optimization_level}"]
+    args += ["-I"] if interpret else []
+
+    pytest.main(args)
+
+
+def test_all_all():
+    args = default_args[:]
+
+    for optimization_level in ["0", "1", "2"]:
+        print(f"Optimization level {optimization_level}, compiling")
+        pytest.main(args + [f"--optimization_level={optimization_level}"])
+
+    for optimization_level in ["0", "1", "2"]:
+        print(f"Optimization level {optimization_level}, interpreting")
+        pytest.main(args + [f"--optimization_level={optimization_level}", "-I"])
+
+
+def main():
+    parser = ArgumentParser(prog="Pl0COM tester", description="Tester for the optimizing compiler for the (modified) PL/0 language", epilog="")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument('-t', '--test', help="Test a single file (can omit the 'test_' prefix)")
+    group.add_argument('-c', '--category', help="Test a single class (can omit the 'Test' prefix)")
+    group.add_argument('-d', '--directory', help="Test all tests in a directory (path can be relative to the project root)")
+    group.add_argument('-a', '--all', default=False, action='store_true', help="Test all tests with a specific optimization level and either compiler or interpreter")
+    group.add_argument('-A', '--all_all', default=False, action='store_true', help="Test all tests with all possible optimization levels and both compiler and interpreter")
+
+    parser.add_argument('-O', '--optimization_level', default="2", choices=["0", "1", "2"])
+    parser.add_argument('-I', '--interpret', default=False, action='store_true')
+    parser.add_argument('-q', '--quiet', default=False, action='store_true', help="Only considered if testing a single test (-t), otherwise automatically set to true")
+
+    args = parser.parse_args()
+
+    if args.test is not None:
+        print(f"TESTING {args.test}")
+        print(f"Optimization level {args.optimization_level}, {'compiling' if not args.interpret else 'interpreting'}")
+        single_test(args.test, args.optimization_level, args.interpret, args.quiet)
+        return
+
+    if args.category is not None:
+        print(f"TESTING CATEGORY {args.category}")
+        print(f"Optimization level {args.optimization_level}, {'compiling' if not args.interpret else 'interpreting'}")
+        single_category(args.category, args.optimization_level, args.interpret)
+        return
+
+    if args.directory is not None:
+        print(f"TESTING DIRECTORY {args.directory}")
+        print(f"Optimization level {args.optimization_level}, {'compiling' if not args.interpret else 'interpreting'}")
+        single_directory(args.directory, args.optimization_level, args.interpret)
+        return
+
+    elif args.all:
+        print("TESTING ALL")
+        print(f"Optimization level {args.optimization_level}, {'compiling' if not args.interpret else 'interpreting'}")
+        test_all(args.optimization_level, args.interpret)
+        return
+
+    print("FULL TESTING")
+    test_all_all()
+
+
+if __name__ == "__main__":
+    main()
