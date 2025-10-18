36ed9f8997cb15ffd867ec848f16be9ed0b71034
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2025-10-18 18:02:16 +0200

    Added test if_else_with_shared_variable

--- tests/basic_constructs/if_construct/test_if_construct.py
+++ tests/basic_constructs/if_construct/test_if_construct.py
@@ -14,3 +14,7 @@ class TestIfConstruct():
     def test_switch_with_if_else(self, optimization_level, interpreter, debug_executable):
         output = get_test_output("tests/basic_constructs/if_construct/02.switch_with_if_else/code.pl0", int(optimization_level), interpreter, debug_executable)
         check_expected_output(output, "tests/basic_constructs/if_construct/02.switch_with_if_else/expected")
+
+    def test_if_else_with_shared_variable(self, optimization_level, interpreter, debug_executable):
+        output = get_test_output("tests/basic_constructs/if_construct/03.if_else_with_shared_variable/code.pl0", int(optimization_level), interpreter, debug_executable)
+        check_expected_output(output, "tests/basic_constructs/if_construct/03.if_else_with_shared_variable//expected")
